0103c4437bcd62724fe78db7b9b40b0bafa1a542
some one <karimmumtazan@gmail.com>
2026-08-20 21:42:08 +0700

    first commit

---
+++ mandiri/day1/oprator.js
@@ -0,0 +1,13 @@
+let hargaBuku = 50000;
+let jumlahBeli = 3;
+let ongkir = 10000;
+ 
+let totalBelanja = (hargaBuku * jumlahBeli) + ongkir;
+console.log("Total belanja:", totalBelanja); // Output: Total belanja: 160000
+ 
+let angka = 10;
+angka++; // sekarang angka jadi 11
+console.log("Angka setelah increment:", angka);
+ 
+let sisaKue = 7 % 3; // 7 kue dibagi ke 3 orang, sisanya berapa?
+console.log("Sisa kue:", sisaKue); // Output: Sisa kue: 1
